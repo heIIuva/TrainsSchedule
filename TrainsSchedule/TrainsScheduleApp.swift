585d23d0774c6432fb6625c80184fb0e9fb05fe2
roman <presenceaura@gmail.com>
2024-12-08 01:52:47 +0300

    Initial Commit

---
+++ TrainsSchedule/TrainsScheduleApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrainsScheduleApp.swift
+//  TrainsSchedule
+//
+//  Created by big stepper on 08/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct TrainsScheduleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
