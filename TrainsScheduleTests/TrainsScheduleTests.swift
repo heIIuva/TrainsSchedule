e860912b35db5a20ac58ff28992284e297099e40
roman <presenceaura@gmail.com>
2024-12-11 18:51:56 +0300

    implemeted NetworManager, refactored tests

--- TrainsScheduleTests/TrainsScheduleTests.swift
+++ TrainsScheduleTests/TrainsScheduleTests.swift
@@ -11,79 +11,116 @@ import OpenAPIURLSession
 
 final class TrainsScheduleTests: XCTestCase {
 
-    let contentView = ContentView()
+    let networkManager = NetworkManager()
     
     // ручка /v3.0/search/
-    func testSchedulesBetweenStations() async throws {
-        let expectation = expectation(description: "testSchedulesBetween")
-        let result: () = await contentView.scheduleBetween(from: "c146", to: "c213")
-        XCTAssertNotNil(result)
-        expectation.fulfill()
-        await fulfillment(of: [expectation], timeout: 10)
-    }
-
+    func testSchedulesBetweenStations() async {
+       let expectation = expectation(description: "testSchedulesBetween")
+       do {
+           let result = try await networkManager.scheduleBetween(from: "c146", to: "c213")
+           XCTAssertNotNil(result)
+           expectation.fulfill()
+       } catch {
+           XCTFail(error.localizedDescription)
+       }
+       await fulfillment(of: [expectation], timeout: 10)
+   }
     // ручка /v3.0/schedule/
-    func testStationSchedule() async throws {
+    func testStationSchedule() async {
         let expectation = expectation(description: "testStationSchedule")
-        let result: () = await contentView.stationSchedule(station: "s9613830")
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+            let result = try await networkManager.stationSchedule(station: "s9613830")
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
         await fulfillment(of: [expectation], timeout: 10)
     }
 
     // ручка /v3.0/thread/
-    func testThreadStations() async throws {
+    func testThreadStations() async {
         let expectation = expectation(description: "testThreadStations")
-        let result: () = await contentView.threadStations(uid: "6296x6294x6292x6291_0_9613602_g24_4")
-        
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+            let result = try await networkManager.threadStations(uid: "6296x6294x6292x6291_0_9613602_g24_4")
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
         
         await fulfillment(of: [expectation], timeout: 10)
     }
 
     // ручка /v3.0/nearest_stations/
-    func testNearestStation() async throws {
+    func testNearestStation() async {
         let expectation = expectation(description: "testNearestStations")
-        let result: () = await contentView.nearestStations(lat: 55.665390, lng: 37.777104, distance: 50)
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+            let result = try await networkManager.nearestStations(lat: 55.665390, lng: 37.777104, distance: 50)
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
         await fulfillment(of: [expectation], timeout: 10)
     }
 
     // ручка /v3.0/nearest_settlement/
-    func testNearestSettlement() async throws {
+    func testNearestSettlement() async {
         let expectation = expectation(description: "testNearestSettlement")
-        let result: () = await contentView.nearestSettlement(lat: 55.665390, lng: 37.777104)
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+        let result = try await networkManager.nearestSettlement(lat: 55.665390,
+                                                                lng: 37.777104)
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
         await fulfillment(of: [expectation], timeout: 10)
     }
 
     // ручка /v3.0/carrier/
-    func testCarrier() async throws {
+    func testCarrier() async {
         let expectation = expectation(description: "testCarrier")
-        let result: () = await contentView.carrier(code: "TK", system: .iata)
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+            let result = try await networkManager.carrier(code: "TK", system: .iata)
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
         await fulfillment(of: [expectation], timeout: 10)
     }
 
     // ручка /v3.0/stations_list/
-    func testStationsList() async throws {
+    func testStationsList() async {
         let expectation = expectation(description: "testStationsList")
-        let result: () = await contentView.stationsList()
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+            let result = try await networkManager.stationsList()
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
         await fulfillment(of: [expectation], timeout: 10)
     }
 
     // ручка /v3.0/copyright/
-    func testCopyright() async throws {
+    func testCopyright() async {
         let expectation = expectation(description: "testCopyright")
-        let result: () = await contentView.copyright()
-        XCTAssertNotNil(result)
-        expectation.fulfill()
+        do {
+            let result = try await networkManager.copyright()
+            XCTAssertNotNil(result)
+            expectation.fulfill()
+        } catch {
+            XCTFail(error.localizedDescription)
+        }
+
         await fulfillment(of: [expectation], timeout: 10)
     }
 }
